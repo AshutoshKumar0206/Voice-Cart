9c60e3fb7d07b539e50cd19fe47a32b406780da0
Ashutosh Kumar <ashutoshbokaro2003@gmail.com>
2025-11-13 14:22:33 +0530

    New Changes

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,7 @@ const path = require('path');
 
 dotenv.config();
 
-const allowedOrigins = ['https://walmart-sparkathon-eight.vercel.app', 'http://localhost:3000'];
+const allowedOrigins = ['https://voicecartapp.vercel.app', 'http://localhost:3000'];
 
 app.use(cookieParser())
 app.use(express.json());
